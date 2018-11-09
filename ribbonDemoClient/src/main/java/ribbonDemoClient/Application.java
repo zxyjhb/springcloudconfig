51fb80d0ae66b96975059c4da7f8d69a6f271d96
jihaibo <568025297@qq.com>
2018-11-09 15:59:58 +0800

    cmt

--- ribbonDemoClient/src/main/java/ribbonDemoClient/Application.java
+++ ribbonDemoClient/src/main/java/ribbonDemoClient/Application.java
@@ -2,10 +2,15 @@ package ribbonDemoClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 
 /**
@@ -16,6 +21,8 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class Application {
 	
 	@Bean
@@ -23,6 +30,16 @@ public class Application {
     public RestTemplate initBean() {
         return new RestTemplate();
     }
+	
+	@Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    } 
 	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- ribbonDemoClient/src/main/java/ribbonDemoClient/DemoService.java
+++ ribbonDemoClient/src/main/java/ribbonDemoClient/DemoService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 public class DemoService {
 	
@@ -17,12 +19,23 @@ public class DemoService {
 	 * url呢http://ribbon-demo-server使用的是注册中心的服务名称，加上接口的资源路径
 	 * @return
 	 */
+	
+	@HystrixCommand(fallbackMethod = "homeFailback")
 	public String home() {
 		return restTemplate.getForObject("http://ribbon-demo-server/home", String.class);
 	 } 
-	
+
+	@HystrixCommand(fallbackMethod = "helloFailback")
 	public String hello() {
 		return restTemplate.getForObject("http://ribbon-demo-server/hello", String.class);
+	}
+	
+	public String homeFailback() {
+		return "home failback return default";
+	}
+	
+	public String helloFailback() {
+		return "hello failback return default";
 	}
 
 
